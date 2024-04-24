05af8a236192a68305c642dacb19fc6d785b4ba2
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2024-04-24 09:06:52 +0100

    final commit

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const onSwitch = document.querySelector('#on');
 const radioButtons = document.querySelectorAll('.switch__wrapper input');
 const light = document.querySelector('.light');
 
-// Function to enable light
+// Function to enable the light
 const enableLight = () => {
   light.classList.add('showLight');
   document.querySelector('h1').classList.add('active');
   document.querySelector('.audio').play();
 };
 
-// Function to disable light
+// Function to disable the light
 const disableLight = () => {
   light.classList.remove('showLight');
   document.querySelector('h1').classList.remove('active');
   document.querySelector('.audio').play();
 };
 
-// Function for on/off switch
+// Function to on/off the light
 radioButtons.forEach((button) => {
   button.addEventListener('click', () => {
     if (onSwitch.checked) {
